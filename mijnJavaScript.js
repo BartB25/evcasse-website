dd157f51dc9ecef5c948d9f6192882eac54a1db6
BartB25 <beeckmansb@gmail.com>
2026-01-07 17:53:52 +0100

    updated nav menu mobile

--- mijnJavaScript.js
+++ mijnJavaScript.js
@@ -1,5 +1,5 @@
 // menu open en dichtklappen
 function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu');
+    const navMenu = document.querySelector('.nav-menu-mobile');
     navMenu.classList.toggle('show');
 }
