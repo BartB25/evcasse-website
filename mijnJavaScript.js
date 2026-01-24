05b5913e5dc6864e1a8a8ded2226053e69604335
BartB25 <beeckmansb@gmail.com>
2026-01-24 09:08:32 +0100

    validation form completed

--- mijnJavaScript.js
+++ mijnJavaScript.js
@@ -3,12 +3,3 @@ function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu-mobile');
     navMenu.classList.toggle('show');
 }
-
-// formulier validatie
-function validateForm() {
-  let x = document.forms["contact"]["name"].value;
-  if (x == "") {
-    alert("Vul uw naam in aub");
-    return false;
-  }
-}
